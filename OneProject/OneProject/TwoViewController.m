05e6117de6fe73e3bd90b35ab543500c9788f6b4
+ <+>
2015-12-29 17:04:19 +0800

    xj

--- OneProject/OneProject/TwoViewController.m
+++ OneProject/OneProject/TwoViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "TwoViewController.h"
-
+#import "TwoTableViewCell.h"
+#import "Model.h"
 @interface TwoViewController ()
+
+@property (nonatomic, retain) UITableView *tableView;
+@property (nonatomic, retain) NSMutableArray *allArray;
 
 @end
 
@@ -17,6 +21,45 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    self.tableView = [[UITableView alloc] initWithFrame:self.view.frame style:UITableViewStylePlain];
+    self.tableView.backgroundColor = [UIColor groupTableViewBackgroundColor];
+    self.tableView.separatorColor = [UIColor blackColor];
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    self.tableView.rowHeight = 88.0;
+    
+    [self.view addSubview:self.tableView];
+    [self configTableData];
+}
+- (void)configTableData{
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"Property List" ofType:@".plist"];
+    NSDictionary *dic = [NSDictionary dictionaryWithContentsOfFile:path];
+    NSDictionary *dic1 = dic[@"root"];
+    NSArray *array = dic1[@"list"];
+    self.allArray = [NSMutableArray new];
+    for (NSDictionary *dic2 in array) {
+        Model *model = [[Model alloc] initWithDictionary:dic2];
+        [self.allArray addObject:model];
+    }
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return self.allArray.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    static NSString *cellIndentifier = @"cell";
+    TwoTableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:cellIndentifier];
+    if (cell == nil) {
+        cell = [[TwoTableViewCell alloc]initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:cellIndentifier];
+    }
+    Model *model = self.allArray[indexPath.row];
+    cell.backgroundColor = [UIColor clearColor];
+    cell.model = model;
+    return cell;
+}
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return 4;
 }
 
 - (void)didReceiveMemoryWarning {
